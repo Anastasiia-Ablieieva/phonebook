49331fe3b2509157caaf14240dc5956b0b781310
Anastasiia-Ablieieva <110480323+Anastasiia-Ablieieva@users.noreply.github.com>
2023-06-10 19:00:34 +0000

    Deploying to gh-pages from @ Anastasiia-Ablieieva/goit-react-hw-08-phonebook@a2809286f00185c603485120f3f0f1f9be2e2e16 🚀

---
+++ static/js/936.3fe9e0bc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{4936:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(1087),c="Home_taxt__ycm6n",s=r(3329);function a(){return(0,s.jsx)("div",{children:(0,s.jsxs)("h2",{className:c,children:["Please,",(0,s.jsx)(n.rU,{to:"/register",children:" Register "}),"and create your contact list, or",(0,s.jsx)(n.rU,{to:"/login",children:" Log in "}),"and use your contacts!"]})})}}}]);
+//# sourceMappingURL=936.3fe9e0bc.chunk.js.map
