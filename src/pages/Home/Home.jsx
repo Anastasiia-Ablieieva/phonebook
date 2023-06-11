2bee97104c6f9c370bdf2f4fc0b65a24a5bb1296
Anastasiia-Ablieieva <110480323+Anastasiia-Ablieieva@users.noreply.github.com>
2023-06-12 01:38:52 +0300

    goit-react-hw-08-phonebook

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,13 @@
 import { Link } from "react-router-dom";
 import css from './Home.module.css'
-import Button from '@mui/material/Button';
-
 
 export default function Home() {
     return (
       <div>
         <p className={css.taxt}>{`Please, `}
-            <Button variant="outlined" to="/register"> Register </Button> 
+            <Link to="/register"> Register </Link> 
             {` and create your contact list, or `} 
-            <Button variant="outlined" to="/login"> Log in </Button> 
+            <Link to="/login"> Log in </Link> 
             {` and use your contacts!`}
         </p>
       </div>
