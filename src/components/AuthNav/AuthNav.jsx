a7798ef06295034350fa8c0f3e79aaa734c1028e
Anastasiia-Ablieieva <110480323+Anastasiia-Ablieieva@users.noreply.github.com>
2023-06-13 22:47:15 +0300

    goit-react-hw-08-phonebook

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function AuthNav() {
     return (
         <div>
-            <Link to="/register"> Register</Link>
-            <Link to="/login"> Log in</Link>
+            <Link to="register"> Register</Link>
+            <Link to="login"> Log in</Link>
         </div>
     );
 }
