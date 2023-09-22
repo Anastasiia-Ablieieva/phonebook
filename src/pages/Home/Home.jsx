1b3ba0a602ee475d56a68d8d659d45ccf5379d86
Anastasiia-Ablieieva <110480323+Anastasiia-Ablieieva@users.noreply.github.com>
2023-09-22 22:06:11 +0300

    fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,16 +6,16 @@ export default function Home() {
     return (
       <div>
         <p className={css.taxt}>
-          <Link to="/register"> 
+          <Link to="/register">
             <Button variant="outlined">Register</Button>
-          </Link> 
-          {` and create your contact list!`} 
+          </Link>
+          {`  and create your contact list!`}
         </p>
         <p className={css.taxt}>
           <Link to="/login">
             <Button variant="outlined">Log in</Button>
-          </Link> 
-          {` and use your contacts!`}
+          </Link>
+          {`  and use your contacts!`}
         </p>
       </div>
     );
